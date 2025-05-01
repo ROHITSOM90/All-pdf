d9c4047f6079d126f6e3831e114ec98bc4f9741d
ROHITSOM90 <rohitsom90@gmail.com>
2025-05-01 20:16:35 +0530

    Add files via upload

---
+++ deepseek_bash_20250501_75d825.sh
@@ -0,0 +1,14 @@
+# Initialize Git
+git init
+
+# Add files
+git add .
+
+# Commit changes
+git commit -m "Initial commit"
+
+# Connect to GitHub
+git remote add origin https://github.com/yourusername/yourusername.github.io.git
+
+# Push files
+git push -u origin main
